4a6f90cfd6590e3d298b724c5d96c584f1fe7e80
Daniel Berthereau <Daniel.git@Berthereau.net>
2021-03-22 00:00:00 +0000

    Added a check during install.

--- Module.php
+++ Module.php
@@ -5,8 +5,12 @@ namespace Ldap;
 use Laminas\Mvc\Controller\AbstractController;
 use Laminas\Mvc\MvcEvent;
 use Laminas\View\Renderer\PhpRenderer;
+use Laminas\ServiceManager\ServiceLocatorInterface;
 use Ldap\Form\ConfigForm;
 use Omeka\Module\AbstractModule;
+use Omeka\Module\Exception\ModuleCannotInstallException;
+use Omeka\Mvc\Controller\Plugin\Messenger;
+use Omeka\Stdlib\Message;
 
 class Module extends AbstractModule
 {
@@ -20,6 +24,28 @@ class Module extends AbstractModule
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function install(ServiceLocatorInterface $serviceLocator)
+    {
+        $this->setServiceLocator($serviceLocator);
+        $translator = $serviceLocator->get('MvcTranslator');
+
+        if (PHP_VERSION_ID < 70300) {
+            $message = $translator->translate('This module requires PHP version 7.3 or higher.'); // @translate
+            throw new ModuleCannotInstallException($message);
+        }
+
+        if (!extension_loaded('ldap')) {
+            $message = $translator->translate('This module requires PHP Ldap extension, which is not loaded.'); // @translate
+            throw new ModuleCannotInstallException($message);
+        }
+
+        $messenger = new Messenger;
+        $message = new Message(
+            $translator->translate('The config of this module should be completed in the main config file. See readme.') // @translate
+        );
+        $messenger->addWarning($message);
     }
 
     public function getConfigForm(PhpRenderer $renderer)
